e328f0d293d9253efee5bcce46f7b67b7bb55d71
Kecia Orleans <keciaorleans@gmail.com>
2024-04-29 19:15:40 +0100

    Merge proofing branch into Kecia's branch

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+<<<<<<< HEAD
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
+=======
+let homeworkRouter = require('./routes/homework'); //for the homework.js file in the route folder
+const studentRouter = require('./routes/student');
+>>>>>>> proofing
 
 const app = express();
 
@@ -16,12 +21,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+<<<<<<< HEAD
 app.use('/', indexRouter);
 app.use("/api/auth", authRouter);
 
+=======
+app.use('/homework', homeworkRouter); 
+app.use('/api/student', studentRouter);
+>>>>>>> proofing
 
 app.get("/", function(req, res, next) {
     res.send("Access the API at path /student");
   });
 
+<<<<<<< HEAD
+=======
+  app.use((req, res, next) => {
+    console.log('Received request:', req.method, req.url);
+    next();
+});
+
+app.get("/", function(req, res, next) {
+  res.send("Access the API at path /student");
+});
+
+
+
+
+
+
+>>>>>>> proofing
 module.exports = app;
