2720ffaa58489c44cfb91f87e31b5a109619d40c
Kecia Orleans <keciaorleans@gmail.com>
2024-04-29 19:35:21 +0100

    Resolved conflicts in app.js and package.json files

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-<<<<<<< HEAD
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
-=======
 let homeworkRouter = require('./routes/homework'); //for the homework.js file in the route folder
 const studentRouter = require('./routes/student');
->>>>>>> proofing
 
 const app = express();
 
@@ -21,34 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
 app.use('/', indexRouter);
 app.use("/api/auth", authRouter);
-
-=======
 app.use('/homework', homeworkRouter); 
 app.use('/api/student', studentRouter);
->>>>>>> proofing
 
-app.get("/", function(req, res, next) {
-    res.send("Access the API at path /student");
-  });
-
-<<<<<<< HEAD
-=======
-  app.use((req, res, next) => {
-    console.log('Received request:', req.method, req.url);
-    next();
+app.use((req, res, next) => {
+  console.log('Received request:', req.method, req.url);
+  next();
 });
 
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /student");
 });
 
-
-
-
-
-
->>>>>>> proofing
 module.exports = app;
