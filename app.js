28257944eaf486e31f10efb95b8af5dfce599e43
Kecia Orleans <keciaorleans@gmail.com>
2024-04-29 19:51:20 +0100

    Minor changes to app.js

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-<<<<<<< HEAD
-=======
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
->>>>>>> kecia
 let homeworkRouter = require('./routes/homework'); //for the homework.js file in the route folder
 const studentRouter = require('./routes/student');
 
@@ -21,28 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
-app.use('/homework', homeworkRouter); 
-app.use('/api/student', studentRouter);
-
-app.get("/", function(req, res, next) {
-    res.send("Access the API at path /homework");
-  });
-
-  app.use((req, res, next) => {
-    console.log('Received request:', req.method, req.url);
-    next();
-});
-
-app.get("/", function(req, res, next) {
-  res.send("Access the API at path /student");
-});
-
-
-
-
-
-=======
 app.use('/', indexRouter);
 app.use("/api/auth", authRouter);
 app.use('/homework', homeworkRouter); 
@@ -56,6 +31,5 @@ app.use((req, res, next) => {
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /student");
 });
->>>>>>> kecia
 
 module.exports = app;
