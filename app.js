003b869fcc9ee1a13387f721484a3115d9161e14
Debra Kinzley <debkinzley@gmail.com>
2024-05-01 12:49:38 -0700

    adding stuff

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const cors = require('cors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-let homeworkRouter = require('./routes/homework'); //for the homework.js file in the route folder
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const homeworkRouter = require('./routes/homework');
 const studentRouter = require('./routes/student');
 
 const app = express();
 
-app.use(cors()); 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,24 +19,24 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use("/api/auth", authRouter);
-app.use('/homework', homeworkRouter); 
+app.use('/homework', homeworkRouter);
 app.use('/api/student', studentRouter);
 
 app.use((req, res, next) => {
   console.log('Received request:', req.method, req.url);
   next();
-app.get("/", function(req, res, next) {
-    res.send("Access the API at path /homework");
-  });
-
-app.use('/api/homework', homeworkRouter); 
-  app.use((req, res, next) => {
-    console.log('Received request:', req.method, req.url);
-    next();
 });
 
 app.get("/", function(req, res, next) {
-  res.send("Access the API at path /student");
+  res.send("Access the API at path /homework");
+});
+
+app.get("/api/homework", function(req, res, next) {
+  res.send("Access the homework API");
+});
+
+app.get("/api/student", function(req, res, next) {
+  res.send("Access the student API");
 });
 
 module.exports = app;
