dc760e5d9dbfb364a5dc33c95d5219b30a9b0b68
AlaaHM500 <146434086+AlaaHM500@users.noreply.github.com>
2024-05-01 19:29:12 +0200

    added new stuff

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 let homeworkRouter = require('./routes/homework'); //for the homework.js file in the route folder
 const studentRouter = require('./routes/student');
 
@@ -16,29 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use("/api/auth", authRouter);
 app.use('/homework', homeworkRouter); 
 app.use('/api/student', studentRouter);
 
-app.get("/", function(req, res, next) {
-    res.send("Access the API at path /homework");
-  });
-
-<<<<<<< HEAD
-app.use('/api/homework', homeworkRouter); 
-=======
-  app.use((req, res, next) => {
-    console.log('Received request:', req.method, req.url);
-    next();
+app.use((req, res, next) => {
+  console.log('Received request:', req.method, req.url);
+  next();
 });
 
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /student");
 });
-
-
->>>>>>> proofing
-
-
-
 
 module.exports = app;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import React from 'react';// import React library to use its features
+import React, { useState } from 'react';// import React library to use its features
 import './App.css';  // import CSS file for styling
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // import components from react-router-dom for routing
 import Subject from './components/Subject'; // import a component called Subject
 import Dashboard from './components/Dashboard'; // import a component called Dashboard
+import Login from './components/Login';
+import RegisterNewStudent from './components/RegisterNewStudent';
 
 
 function App() {
   // get today's date and format it
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const today = new Date().toLocaleDateString(undefined, options);
- 
+
+  const [ userData, setUserData ] = useState(null);
+  
   return (
     <Router>{/* Router component is used to wrap all our routes */}
       <div>
             {/* Today's date */}
         <div className="date">{today}</div>
-<<<<<<< HEAD
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/:subject" element={<Subject />} /> 
-          {/* what comes after the route, you can grab values - you can create variables when you use the colon */}
-=======
 
 
         <Routes>{/* Routes component holds all our defined routes */}
-          <Route path="/" element={<Dashboard />} /> {/* Route for Dashboard, which is shown when the URL is / */}
-          <Route path="/:subject" element={<Subject />} /> {/* Route for the Subject component, which is shown when the URL has something after /, like /math or /science */}
+          <Route path="/" element={<Login setUserData = { setUserData }/>} /> 
+          {/*Route for main page, which features a login form and the link to registration page if student is not added to database*/}
+          <Route path="/RegisterNewStudent" element={<RegisterNewStudent />} />
+          <Route path="/dashboard" element={<Dashboard userData = { userData } />} /> 
+          {/* Route for Dashboard, which is shown when the student is logged in */}
+          <Route path="/:subject" element={<Subject />} /> 
+          {/* Route for the Subject component, which is shown when the URL has something after /, like /math or /science */}
           {/* :subject captures whatever comes after the route, so you can grab values from the URL */}
->>>>>>> proofing
-          
         </Routes>
     
       </div>
