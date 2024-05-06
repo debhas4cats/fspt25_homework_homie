43ea25e4eeaec18f1a9c7ed4a971fe45c1f29396
Debra Kinzley <debkinzley@gmail.com>
2024-05-05 20:59:14 -0700

    added notes

--- client/src/components/ClickableDate.jsx
+++ client/src/components/ClickableDate.jsx
@@ -196,6 +196,7 @@ function ClickableDate() {
 
   // Render the component
   return (
+
     <div className="clickable-date">
       <div>
         {/* Conditional rendering for the HOME and date buttons */}
